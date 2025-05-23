43c364e86c1326f3ca7fcff6fc79135ab9a330ec
Robert <Robmmar90@gmail.com>
2025-05-23 15:04:12 -0700

    updated player class with groups

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta_time = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable) # type: ignore
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        updateable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        
     
     
 if __name__ == "__main__":
